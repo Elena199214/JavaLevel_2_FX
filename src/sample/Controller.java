332aef547d918ffb0077612a189af4b88ca64cb9
Gudkova Lena <gudkova_elena1992@mail.ru>
2021-05-12 07:45:55 +0300

    в работе

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,28 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 
+
+public class Controller {
+    @FXML
+    TextArea mainTextArea;
+    public void sendOneClickAction(ActionEvent actionEvent) {
+        mainTextArea.appendText("1\n");
+
+        //Button thisButton = (Button)actionEvent.getSource(); // Вот так можно получить ссылку на кнопку,
+        // которая была нажата (например, есл и вы одно и то же действие повесили на десяток кнопок)
+    }
+
+
+
+
+
+//
+//
+//    public void sendOneClickAction(ActionEvent actionEvent) {
+//        mainTextArea.appendText("1\n");
+//    }
 }
